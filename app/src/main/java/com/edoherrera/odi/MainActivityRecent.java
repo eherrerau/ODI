4b7836d9efab84bd58bb97e4dee8bc36387f8b8a
Eduardo Herrera <eherrerau@gmail.com>
2016-01-03 22:10:57 -0600

    Created the single_row_recent_activity_recent.xml file, wich contatins the layour for the row.  Also, added dialpad, new black, account, phone, phone_forward and timeelapse icons. (Some in XML vectors, and other as png)

--- app/src/main/java/com/edoherrera/odi/MainActivityRecent.java
+++ app/src/main/java/com/edoherrera/odi/MainActivityRecent.java
@@ -82,13 +82,13 @@ public class MainActivityRecent extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_recent) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_dial) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_myCards) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_newCard) {
 
         } else if (id == R.id.nav_share) {
 
